7aa6c65a7b00f1116c72499537b43711e76153bb
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-10-09 11:28:30 -0400

    added validation to commands to retrived files

--- vendor_file_cli/commands.py
+++ vendor_file_cli/commands.py
@@ -4,8 +4,10 @@ import logging
 import logging.handlers
 import datetime
 import os
-import yaml
 from file_retriever.connect import Client
+from file_retriever.file import FileInfo
+from vendor_file_cli.validator import validate_file
+
 
 logger = logging.getLogger("vendor_file_cli")
 
@@ -32,7 +34,10 @@ def connect(name: str) -> Client:
 
 
 def get_vendor_files(
-    vendors: list[str], days: int = 0, hours: int = 0, minutes: int = 0
+    vendors: list[str],
+    days: int = 0,
+    hours: int = 0,
+    minutes: int = 0,
 ) -> None:
     """
     Retrieve files from remote server for vendors in `vendor_list`. Forms timedelta
@@ -40,7 +45,8 @@ def get_vendor_files(
     that time delta. If days, hours, and minutes args are not provided, all files that
     are in the vendor's remote directory will be included in the list. Compares that
     list of files to the list of files in the vendor's NSDROP directory only copies
-    the files that are not already present in the NSDROP directory.
+    the files that are not already present in the NSDROP directory. Will validate files
+    before copying if validate is True.
 
     Args:
         vendors: list of vendor names
@@ -52,89 +58,62 @@ def get_vendor_files(
         None
 
     """
-    nsdrop_client = connect("nsdrop")
+    nsdrop = connect("nsdrop")
     timedelta = datetime.timedelta(days=days, hours=hours, minutes=minutes)
-
     for vendor in vendors:
-        vendor_src_dir = os.environ[f"{vendor.upper()}_SRC"]
-        vendor_dst_dir = os.environ[f"{vendor.upper()}_DST"]
         with connect(vendor) as client:
             file_list = client.list_file_info(
                 time_delta=timedelta,
-                remote_dir=vendor_src_dir,
+                remote_dir=os.environ[f"{vendor.upper()}_SRC"],
             )
-            files_to_add = nsdrop_client.check_file_list(
-                files=file_list, dir=vendor_dst_dir, remote=True
+            files = nsdrop.check_file_list(
+                files=file_list, dir=os.environ[f"{vendor.upper()}_DST"], remote=True
             )
-            if len(files_to_add) == 0:
-                logger.debug(
-                    f"({vendor.upper()}) NSDROP directory is already up to date"
+            for file in files:
+                get_single_file(
+                    vendor=vendor,
+                    file=file,
+                    vendor_client=client,
+                    nsdrop_client=nsdrop,
                 )
-                continue
-            logger.debug(f"Writing {len(files_to_add)} files to NSDROP directory")
-            for file in files_to_add:
-                fetched_file = client.get_file(file=file, remote_dir=vendor_src_dir)
-                nsdrop_client.put_file(
-                    file=fetched_file,
-                    dir=vendor_dst_dir,
-                    remote=True,
-                    check=True,
-                )
-    nsdrop_client.close()
+    nsdrop.close()
 
 
-def load_vendor_creds(config_path: str) -> list[str]:
-    """
-    Read config file with credentials and set creds as environment variables.
-    Returns a list of vendors whose FTP/SFTP credentials are stored in the
-    config file and have been added to env vars. NSDROP is excluded from this list.
-
-    Args:
-        config_path (str): Path to the yaml file with credendtials.
-
-    Returns:
-        list of names of servers (eg. EASTVIEW, LEILA) whose credentials are
-        stored in the config file and have been added to env vars
-    """
-    with open(config_path, "r") as file:
-        config = yaml.safe_load(file)
-        if config is None:
-            raise ValueError("No credentials found in config file.")
-        vendor_list = [
-            i.split("_HOST")[0]
-            for i in config.keys()
-            if i.endswith("_HOST") and "NSDROP" not in i
-        ]
-        for k, v in config.items():
-            os.environ[k] = v
-        for vendor in vendor_list:
-            os.environ[f"{vendor}_DST"] = f"NSDROP/vendor_records/{vendor.lower()}"
-        return vendor_list
-
-
-def logger_config(app_logger: logging.Logger) -> None:
-    """
-    Create and return dict for logger configuration.
-    """
-    root_logger = logging.getLogger("file_retriever")
-    root_logger.setLevel(logging.DEBUG)
-    app_logger.setLevel(logging.DEBUG)
-
-    stream_handler = logging.StreamHandler()
-    file_handler = logging.handlers.RotatingFileHandler(
-        filename="vendor_file_cli.log",
-        maxBytes=10 * 1024 * 1024,
-        backupCount=5,
-        encoding="utf8",
+def get_single_file(
+    vendor: str, file: FileInfo, vendor_client: Client, nsdrop_client: Client
+) -> None:
+    fetched_file = vendor_client.get_file(
+        file=file, remote_dir=os.environ[f"{vendor.upper()}_SRC"]
     )
-    formatter = logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(message)s")
+    if vendor.upper() in ["EASTVIEW", "LEILA", "AMALIVRE_SASB"]:
+        logger.info(
+            f"({nsdrop_client.name}) Validating {vendor} file: {fetched_file.file_name}"
+        )
+        validate_file(file_obj=fetched_file, vendor=vendor, write=True)
+    nsdrop_client.put_file(
+        file=fetched_file,
+        dir=os.environ[f"{vendor.upper()}_DST"],
+        remote=True,
+        check=True,
+    )
 
-    stream_handler.setLevel(logging.DEBUG)
-    stream_handler.setFormatter(formatter)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
 
-    app_logger.addHandler(stream_handler)
-    app_logger.addHandler(file_handler)
-    root_logger.addHandler(stream_handler)
-    root_logger.addHandler(file_handler)
+def validate_files(vendor: str, files: list | None) -> None:
+    file_dir = os.environ[f"{vendor.upper()}_DST"]
+    vendor_file_list = []
+    with connect("nsdrop") as nsdrop_client:
+        if files and files is not None:
+            for file in files:
+                vendor_file_list.append(
+                    nsdrop_client.get_file_info(file_name=file, remote_dir=file_dir)
+                )
+        else:
+            vendor_file_list.extend(nsdrop_client.list_file_info(remote_dir=file_dir))
+    for file in vendor_file_list:
+        client = connect("nsdrop")
+        file_obj = client.get_file(file=file, remote_dir=file_dir)
+        logger.info(
+            f"({nsdrop_client.name}) Validating {vendor} file: {file_obj.file_name}"
+        )
+        validate_file(file_obj=file_obj, vendor=vendor, write=True)
+        client.close()
