b8bf055a4d3426261005d86d4b8bbcfc2eb96772
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-10-09 11:29:14 -0400

    added fixtures for mock marc records

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,164 @@
 import io
+import logging
+import os
+from pymarc import Record, Field, Subfield
 import pytest
 from click.testing import CliRunner
 from file_retriever.connect import Client
 from file_retriever.file import File, FileInfo
 
+logger = logging.getLogger("vendor_file_cli")
 
-def mock_file_info() -> FileInfo:
+
+def stub_marc():
+    bib = Record()
+    bib.leader = "00454cam a22001575i 4500"
+    bib.add_field(Field(tag="001", data="on1381158740"))
+    bib.add_field(Field(tag="008", data="190306s2017    ht a   j      000 1 hat d"))
+    bib.add_field(
+        Field(
+            tag="050",
+            indicators=[" ", "4"],
+            subfields=[
+                Subfield(code="a", value="F00"),
+            ],
+        )
+    )
+    bib.add_field(
+        Field(
+            tag="245",
+            indicators=["0", "0"],
+            subfields=[
+                Subfield(code="a", value="Title :"),
+                Subfield(
+                    code="b",
+                    value="subtitle /",
+                ),
+                Subfield(
+                    code="c",
+                    value="Author",
+                ),
+            ],
+        )
+    )
+    bib.add_field(
+        Field(
+            tag="300",
+            indicators=[" ", " "],
+            subfields=[
+                Subfield(code="a", value="100 pages :"),
+            ],
+        )
+    )
+    bib.add_field(
+        Field(
+            tag="852",
+            indicators=["8", " "],
+            subfields=[
+                Subfield(code="h", value="ReCAP 23-100000"),
+            ],
+        )
+    )
+    bib.add_field(
+        Field(
+            tag="901",
+            indicators=[" ", " "],
+            subfields=[
+                Subfield(code="a", value="EVP"),
+            ],
+        )
+    )
+    bib.add_field(
+        Field(
+            tag="910",
+            indicators=[" ", " "],
+            subfields=[
+                Subfield(code="a", value="RL"),
+            ],
+        )
+    )
+    bib.add_field(
+        Field(
+            tag="949",
+            indicators=[" ", "1"],
+            subfields=[
+                Subfield(code="z", value="8528"),
+                Subfield(code="a", value="ReCAP 23-100000"),
+                Subfield(code="c", value="1"),
+                Subfield(code="h", value="43"),
+                Subfield(code="i", value="33433123456789"),
+                Subfield(code="l", value="rcmf2"),
+                Subfield(code="m", value="bar"),
+                Subfield(code="p", value="1.00"),
+                Subfield(code="t", value="55"),
+                Subfield(code="u", value="foo"),
+                Subfield(code="v", value="EVP"),
+            ],
+        )
+    )
+    bib.add_field(
+        Field(
+            tag="960",
+            indicators=[" ", " "],
+            subfields=[
+                Subfield(code="s", value="100"),
+                Subfield(code="t", value="MAF"),
+                Subfield(code="u", value="123456apprv"),
+            ],
+        )
+    )
+    bib.add_field(
+        Field(
+            tag="980",
+            indicators=[" ", " "],
+            subfields=[
+                Subfield(code="a", value="240101"),
+                Subfield(code="b", value="100"),
+                Subfield(code="c", value="100"),
+                Subfield(code="d", value="000"),
+                Subfield(code="e", value="200"),
+                Subfield(code="f", value="123456"),
+                Subfield(code="g", value="1"),
+            ],
+        )
+    )
+    return bib
+
+
+@pytest.fixture
+def stub_record():
+    return stub_marc()
+
+
+@pytest.fixture
+def stub_file_info() -> FileInfo:
+    return mock_file_info(file_name="foo.mrc")
+
+
+def mock_file(
+    file_name: str | None = None,
+) -> File:
+    if file_name is None:
+        file_name = "foo.mrc"
+    marc_data = stub_marc()
+    record = marc_data.as_marc21()
+    return File(
+        file_name=file_name,
+        file_mtime=1704070800,
+        file_mode=33188,
+        file_atime=None,
+        file_gid=0,
+        file_uid=0,
+        file_size=140401,
+        file_stream=io.BytesIO(record),
+    )
+
+
+def mock_file_info(file_name: str | None = None) -> FileInfo:
+    if file_name is None:
+        file_name = "foo.mrc"
     return FileInfo(
-        file_name="foo.mrc",
+        file_name=file_name,
         file_mtime=1704070800,
         file_mode=33188,
         file_atime=None,
@@ -24,20 +175,41 @@ class MockClient:
     def close(self) -> None:
         pass
 
-    def fetch_file(self, *args, **kwargs) -> File:
-        return File.from_fileinfo(file=mock_file_info(), file_stream=io.BytesIO(b"foo"))
+    def fetch_file(self, file, *args, **kwargs) -> File:
+        if isinstance(file, str):
+            return mock_file(file_name=file)
+        elif isinstance(file, FileInfo):
+            marc_data = stub_marc()
+            return File.from_fileinfo(file, io.BytesIO(marc_data.as_marc21()))
+        elif isinstance(file, File):
+            return file
+        else:
+            return mock_file(file_name=None)
 
-    def get_file_data(self, *args, **kwargs) -> FileInfo:
-        return mock_file_info()
+    def get_file_data(self, file_name, *args, **kwargs) -> FileInfo:
+        if isinstance(file_name, str):
+            return mock_file_info(file_name=file_name)
+        elif isinstance(file_name, FileInfo):
+            return file_name
+        else:
+            return mock_file_info(file_name=None)
 
     def is_active(self) -> bool:
         return True
 
     def list_file_data(self, *args, **kwargs) -> list[FileInfo]:
-        return [mock_file_info()]
+        return [mock_file_info(file_name=None)]
 
-    def write_file(self, *args, **kwargs) -> FileInfo:
-        return mock_file_info()
+    def write_file(self, file, *args, **kwargs) -> FileInfo:
+        if isinstance(file, str):
+            return mock_file(file_name=file)
+        elif isinstance(file, FileInfo):
+            marc_data = stub_marc()
+            return File.from_fileinfo(file, io.BytesIO(marc_data.as_marc21()))
+        elif isinstance(file, File):
+            return file
+        else:
+            return mock_file(file_name=None)
 
 
 @pytest.fixture
@@ -50,6 +222,32 @@ def mock_Client(monkeypatch):
 
     monkeypatch.setattr(Client, "check_file", mock_check_file)
     monkeypatch.setattr(Client, "_Client__connect_to_server", mock_session)
+
+
+@pytest.fixture
+def mock_creds():
+    (
+        os.environ["NSDROP_HOST"],
+        os.environ["NSDROP_USER"],
+        os.environ["NSDROP_PASSWORD"],
+        os.environ["NSDROP_PORT"],
+        os.environ["NSDROP_SRC"],
+    ) = ("sftp.foo.com", "NSDROP", "nsdrop", "22", "nsdrop_src")
+    (
+        os.environ["EASTVIEW_HOST"],
+        os.environ["EASTVIEW_USER"],
+        os.environ["EASTVIEW_PASSWORD"],
+        os.environ["EASTVIEW_PORT"],
+        os.environ["EASTVIEW_SRC"],
+        os.environ["EASTVIEW_DST"],
+    ) = (
+        "sftp.foo.com",
+        "eastview",
+        "evp",
+        "22",
+        "eastview_src",
+        "NSDROP/vendor_records/eastview",
+    )
 
 
 @pytest.fixture
@@ -80,7 +278,7 @@ def mock_load_vendor_creds(monkeypatch, mock_open_yaml_file):
         return ["FOO", "BAR", "BAZ", "NSDROP"]
 
     monkeypatch.setattr(
-        "vendor_file_cli.commands.load_vendor_creds", mock_load_vendor_creds
+        "vendor_file_cli.config.load_vendor_creds", mock_load_vendor_creds
     )
     monkeypatch.setenv("USERPROFILE", "test")
     monkeypatch.setattr("os.path.join", mock_path)
