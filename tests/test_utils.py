f3945e3adb9d30c06fb0557687da0770e0384635
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-10-24 14:40:00 -0400

    fixed utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,11 +2,18 @@ import logging
 import logging.config
 import logging.handlers
 import os
+from pymarc import Field, Subfield
 import pytest
+from file_retriever.connect import Client
 from vendor_file_cli.utils import (
     configure_logger,
+    configure_sheet,
+    connect,
     create_logger_dict,
+    get_control_number,
     load_vendor_creds,
+    read_marc_file_stream,
+    write_data_to_sheet,
 )
 
 
@@ -54,6 +61,39 @@ def test_configure_logger():
     assert sorted([i.level for i in config_handlers]) == sorted([10, 20])
 
 
+def test_configure_sheet_success(mock_sheet_config):
+    creds = configure_sheet()
+    assert creds.token == "foo"
+    assert creds.valid is True
+    assert creds.expired is False
+    assert creds.refresh_token is not None
+
+
+def test_configure_sheet_invalid(mock_sheet_config_creds_invalid):
+    creds = configure_sheet()
+    assert creds.token == "foo"
+    assert creds.valid is True
+    assert creds.expired is False
+    assert creds.refresh_token is not None
+
+
+def test_configure_sheet_generate_new_creds(mock_sheet_config_no_creds):
+    creds = configure_sheet()
+    assert creds.token == "foo"
+    assert creds.valid is True
+    assert creds.expired is False
+    assert creds.refresh_token is not None
+
+
+def test_connect(mock_Client):
+    client = connect("leila")
+    assert client.name == "LEILA"
+    assert client.host == "ftp.leila.com"
+    assert client.port == "21"
+    assert isinstance(client, Client)
+    assert client.session is not None
+
+
 def test_create_logger_dict():
     logger_dict = create_logger_dict()
     assert sorted(list(logger_dict.keys())) == sorted(
@@ -66,6 +106,42 @@ def test_create_logger_dict():
     assert sorted(list(logger_dict["loggers"].keys())) == sorted(
         ["file_retriever", "file_retriever.vendor_file_cli"]
     )
+
+
+def test_get_control_number(stub_record):
+    control_no = get_control_number(stub_record)
+    assert control_no == "on1381158740"
+
+
+@pytest.mark.parametrize(
+    "field",
+    ["020", "035", "022", "024", "010"],
+)
+def test_get_control_number_other_tag(stub_record, field):
+    stub_record.remove_fields("001")
+    stub_record.add_ordered_field(
+        Field(
+            tag=field,
+            indicators=[" ", " "],
+            subfields=[
+                Subfield(code="a", value="foo"),
+            ],
+        )
+    )
+    control_no = get_control_number(stub_record)
+    assert control_no == "foo"
+
+
+def test_get_control_number_call_no(stub_record):
+    stub_record.remove_fields("001")
+    control_no = get_control_number(stub_record)
+    assert control_no == "ReCAP 23-100000"
+
+
+def test_get_control_number_none(stub_record):
+    stub_record.remove_fields("001", "852")
+    control_no = get_control_number(stub_record)
+    assert control_no == "None"
 
 
 def test_load_vendor_creds(mocker):
@@ -103,3 +179,46 @@ def test_load_vendor_creds_empty_yaml(mocker):
     with pytest.raises(ValueError) as exc:
         load_vendor_creds("foo.yaml")
     assert "No credentials found in config file" in str(exc.value)
+
+
+def test_read_marc_file_stream(stub_file):
+    stream = read_marc_file_stream(stub_file)
+    assert stream is not None
+    assert stream.__next__().get_fields("001")[0].data == "on1381158740"
+    records = [i for i in read_marc_file_stream(stub_file)]
+    assert len(records) == 1
+
+
+def test_write_data_to_sheet(mock_sheet_config):
+    data = write_data_to_sheet(
+        values={"file_name": ["foo.mrc"], "vendor_code": ["FOO"]}
+    )
+    assert sorted(list(data.keys())) == sorted(
+        [
+            "spreadsheetId",
+            "tableRange",
+        ]
+    )
+
+
+def test_write_data_to_sheet_http_error(
+    mock_sheet_config, mock_sheet_http_error, caplog
+):
+    data = write_data_to_sheet(
+        values={"file_name": ["foo.mrc"], "vendor_code": ["FOO"]}
+    )
+    assert data is None
+    assert "Error occurred while sending data to google sheet: " in caplog.text
+
+
+def test_write_data_to_sheet_timeout_error(
+    mock_sheet_config, mock_sheet_timeout_error, caplog
+):
+    data = write_data_to_sheet(
+        values={"file_name": ["foo.mrc"], "vendor_code": ["FOO"]}
+    )
+    assert data is None
+    assert (
+        "Error occurred while sending data to google sheet: Connection timed out"
+        in caplog.text
+    )
--- vendor_file_cli/utils.py
+++ vendor_file_cli/utils.py
@@ -2,6 +2,16 @@ import logging
 import logging.config
 import os
 import yaml
+from typing import Generator
+from googleapiclient.discovery import build  # type: ignore
+from googleapiclient.errors import HttpError  # type: ignore
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow  # type: ignore
+import pandas as pd
+from pymarc import MARCReader, Record
+from file_retriever.connect import Client
+from file_retriever.file import File
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +21,55 @@ def configure_logger(logger_dict: dict) -> None:
     loggers = [i for i in logger_dict["loggers"].keys()]
     for logger in loggers:
         logging.getLogger(f"{logger}")
+
+
+def configure_sheet() -> Credentials:
+    """
+    Get or update credentials for google sheets API and save token to file.
+
+    Args:
+        None
+
+    Returns:
+        google.oauth2.credentials.Credentials: Credentials object for google sheet API.
+    """
+    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+    cred_path = os.path.join(
+        os.environ["USERPROFILE"], ".cred/.google/desktop-app.json"
+    )
+    token_path = os.path.join(os.environ["USERPROFILE"], ".cred/.google/token.json")
+
+    creds = Credentials.from_authorized_user_file(token_path, scopes)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(cred_path, scopes)
+            creds = flow.run_local_server()
+        with open(token_path, "w") as token:
+            token.write(creds.to_json())
+    return creds
+
+
+def connect(name: str) -> Client:
+    """
+    Create and return a `Client` object for the specified server
+    using credentials stored in env vars.
+
+    Args:
+        name: name of server (eg. EASTVIEW, NSDROP)
+
+    Returns:
+        a `Client` object for the specified server
+    """
+    client_name = name.upper()
+    return Client(
+        name=client_name,
+        username=os.environ[f"{client_name}_USER"],
+        password=os.environ[f"{client_name}_PASSWORD"],
+        host=os.environ[f"{client_name}_HOST"],
+        port=os.environ[f"{client_name}_PORT"],
+    )
 
 
 def create_logger_dict() -> dict:
@@ -63,6 +122,31 @@ def create_logger_dict() -> dict:
     }
 
 
+def get_control_number(record: Record) -> str:
+    """Get control number from MARC record to output to google sheet."""
+    field = record.get("001", None)
+    if field is not None:
+        control_number = field.data
+        if control_number is not None:
+            return control_number
+    field_subfield_pairs = [
+        ("035", "a"),
+        ("020", "a"),
+        ("010", "a"),
+        ("022", "a"),
+        ("024", "a"),
+        ("852", "h"),
+    ]
+    for f, s in field_subfield_pairs:
+        while record.get(f, None) is not None:
+            field = record.get(f, None)
+            if field is not None:
+                subfield = field.get(s)
+                if subfield is not None:
+                    return subfield
+    return "None"
+
+
 def load_vendor_creds(config_path: str) -> list[str]:
     """
     Read config file with credentials and set creds as environment variables.
@@ -90,3 +174,63 @@ def load_vendor_creds(config_path: str) -> list[str]:
         for vendor in vendor_list:
             os.environ[f"{vendor}_DST"] = f"NSDROP/vendor_records/{vendor.lower()}"
         return vendor_list
+
+
+def read_marc_file_stream(file_obj: File) -> Generator[Record, None, None]:
+    """Read the filestream within a File object using pymarc"""
+    fh = file_obj.file_stream.getvalue()
+    reader = MARCReader(fh)
+    for record in reader:
+        yield record
+
+
+def write_data_to_sheet(values: dict) -> dict | None:
+    """"""
+    vendor_code = values["vendor_code"][0]
+    creds = configure_sheet()
+
+    df = pd.DataFrame(
+        values,
+        columns=[
+            "validation_date",
+            "file_name",
+            "vendor_code",
+            "record_number",
+            "control_number",
+            "valid",
+            "error_count",
+            "missing_field_count",
+            "missing_fields",
+            "extra_field_count",
+            "extra_fields",
+            "invalid_field_count",
+            "invalid_fields",
+            "order_item_mismatches",
+        ],
+    )
+    df.fillna("", inplace=True)
+
+    body = {
+        "majorDimension": "ROWS",
+        "range": f"{vendor_code.upper()}!A1:O10000",
+        "values": df.values.tolist(),
+    }
+    try:
+        service = build("sheets", "v4", credentials=creds)
+        result = (
+            service.spreadsheets()
+            .values()
+            .append(
+                spreadsheetId="1ZYuhMIE1WiduV98Pdzzw7RwZ08O-sJo7HJihWVgSOhQ",
+                range=f"{vendor_code.upper()}!A1:O10000",
+                valueInputOption="USER_ENTERED",
+                insertDataOption="INSERT_ROWS",
+                body=body,
+                includeValuesInResponse=True,
+            )
+            .execute()
+        )
+        return result
+    except (HttpError, TimeoutError) as e:
+        logger.error(f"Error occurred while sending data to google sheet: {e}")
+        return None
